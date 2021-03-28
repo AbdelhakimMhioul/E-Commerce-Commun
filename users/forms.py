6228518467368c879752ccd96873d9b5153ba0ad
Hakim1975-cyber <hk.mhl1975@gmail.com>
2021-03-28 18:15:36 +0100

    Eliminated Warnings

--- users/forms.py
+++ users/forms.py
@@ -14,4 +14,4 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = {'username', 'email', 'password1', 'password2'}
+        fields = ['username', 'email', 'password1', 'password2']
